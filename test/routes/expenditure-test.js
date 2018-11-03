b2152f89c50511e71af38aa3c472c4494685c7e5
YING YAN <1804094745@qq.com>
2018-11-03 19:38:22 +0800

    Add expenditure endpoint tested

--- test/routes/expenditure-test.js
+++ test/routes/expenditure-test.js
@@ -8,4 +8,37 @@ let _ = require('lodash' );
 
 describe('Expenditures', function (){
     // TODO
+    describe('POST /expenditures', function () {
+        it('should return confirmation message and update database', function(done) {
+            let expenditure = {
+                username: 'April',
+                date: '2018-12-04',
+                payment: 'Visa card' ,
+                amount: 10,
+                description: 'glasses'
+            };
+            chai.request(server)
+                .post('/expenditures')
+                .send(expenditure)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message').equal('Expenditure Successfully Added!' );
+                    let expenditure = res.body.data;
+                    expect(expenditure).to.include({description: 'glasses', date: '2018-12-04'});
+                    done();
+                });
+        });
+        after(function  (done) {
+            chai.request(server)
+                .get('/expenditures')
+                .end(function(err, res) {
+                    let result = _.map(res.body, (expenditure) => {
+                        return { payment: expenditure.payment,
+                            amount: expenditure.amount };
+                    }  );
+                    expect(result).to.include( { payment: 'Visa card', amount: 10  } );
+                    done();
+                });
+        });  // end-after
+    });
 });
