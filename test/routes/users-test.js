ae443db05cdf62b35e4f4118c7c4d134d81c61e1
YING YAN <1804094745@qq.com>
2018-11-04 02:37:25 +0800

    Add users endpoint tested

--- test/routes/users-test.js
+++ test/routes/users-test.js
@@ -8,4 +8,38 @@ let _ = require('lodash' );
 
 describe('Users', function (){
     // TODO
+    describe('POST /users', function () {
+        it('should return confirmation message and update database', function(done) {
+            let user = {
+                username: 'Amber' ,
+                password: '19293484857'
+                //payment: 'Visa card' ,
+                //amount: 300,
+                //date: '2018-10-18'
+            };
+            chai.request(server)
+                .post('/users')
+                .send(user)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message').equal('User Successfully Added!' );
+                    let user = res.body.data;
+                    expect(user).to.include({username: 'Amber', password: '19293484857'});
+                    done();
+                });
+        });
+        after(function  (done) {
+            chai.request(server)
+                .get('/users')
+                .end(function(err, res) {
+                    let result = _.map(res.body, (user) => {
+                        return { username: user.username,
+                                  password: user.password };
+                    }  );
+                    expect(result).to.include( { username: 'Amber', password: '19293484857'  } );
+                    done();
+                });
+        });  // end-after
+    });
+
 });
