2fe98b88c9525e84c9b7bf0ed1389f5a97583070
YING YAN <1804094745@qq.com>
2018-11-03 19:44:34 +0800

    Updated expenditure endpoint tested

--- test/routes/expenditure-test.js
+++ test/routes/expenditure-test.js
@@ -41,4 +41,53 @@ describe('Expenditures', function (){
                 });
         });  // end-after
     });
+
+
+
+    describe('PUT /expenditures/:id/changeExinfo', () => {
+        describe ('when id is valid',function() {
+            it('should return a confirmation message and update database', function (done) {
+                let expenditure = {
+                    username: 'April',
+                    description: 'glasses and snack',
+                    date: '2018-12-04',
+                    payment: 'Visa card',
+                    amount: 15
+                };
+                chai.request(server)
+                    .put('/expenditures/5bdd88469dd67b3a4c06e09b/changeExinfo')
+                    .send(expenditure)
+                    .end(function (err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.have.property('Message').equal('Expenditure Successfully Changed!');
+                        let expenditure = res.body.data;
+                        expect(expenditure).to.include({description: 'glasses and snack', date: '2018-12-04'});
+                        done();
+                    });
+            });
+            after(function  (done) {
+                chai.request(server)
+                    .get('/expenditures')
+                    .end(function(err, res) {
+                        let result = _.map(res.body, (expenditure) => {
+                            return { description: expenditure.description,
+                                amount: expenditure.amount };
+                        }  );
+                        expect(result).to.include( { description: 'glasses and snack', amount: 15  } );
+                        done();
+                    });
+            });  // end-after
+        });
+        describe('when id is invalid',function() {
+            it('should return a 404 and a message for invalid expenditure id', function (done) {
+                chai.request(server)
+                    .put('/expenditures/1100001/changeExinfo')
+                    .end(function (err, res) {
+                        expect(res).to.have.status(404);
+                        expect(res.body).to.have.property('Message', 'Sorry! Cannot find the expenditure by this id!');
+                        done();
+                    });
+            });
+        });
+    });
 });
