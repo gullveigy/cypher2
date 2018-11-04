72339a32b3e2968929d1e7425e6463fc78ca94c6
YING YAN <1804094745@qq.com>
2018-11-05 03:43:30 +0800

    Get all income records endpoint tested

--- test/routes/income-test.js
+++ test/routes/income-test.js
@@ -6,6 +6,35 @@ chai.use(require('chai-things'));
 chai.use(chaiHttp);
 let _ = require('lodash' );
 
-describe('Donations', function (){
+describe('Incomes', function (){
     // TODO
+    describe('GET /incomes',  () => {
+        it('should return all the income records in an array', function(done) {
+            chai.request(server)
+                .get('/incomes')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(12);
+                    let result = _.map(res.body, (income) => {
+                        return { description: income.description,
+                                  amount: income.amount }
+                    });
+                    expect(result).to.include( { description: 'benefits', amount: 78  });
+                    expect(result).to.include( { description: 'wages', amount: 138  } );
+                    expect(result).to.include( { description: 'wages', amount: 57  } );
+                    expect(result).to.include( { description: 'benefits', amount: 48  } );
+                    expect(result).to.include( { description: 'benefits', amount: 235  } );
+                    expect(result).to.include( { description: 'wages', amount: 580  } );
+                    expect(result).to.include( { description: 'wages', amount: 590  } );
+                    expect(result).to.include( { description: 'wages', amount: 1200  } );
+                    expect(result).to.include( { description: 'prize draw', amount: 46  } );
+                    expect(result).to.include( { description: 'wages', amount: 1178  } );
+                    expect(result).to.include( { description: 'prize draw', amount: 35  } );
+                    expect(result).to.include( { description: 'benefits', amount: 47.5  } );
+                    done();
+                });
+
+        });
+    });
 });
