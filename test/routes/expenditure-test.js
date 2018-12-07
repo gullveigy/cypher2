54038d74a40baace57cd8e9098afae4b9dcf1bdb
YING YAN <1804094745@qq.com>
2018-12-07 10:37:25 +0800

    Travis generates coverage reports for expenditure-test

--- test/routes/expenditure-test.js
+++ test/routes/expenditure-test.js
@@ -9,183 +9,6 @@ chai.use(chaiHttp);
 
 describe('Expenditures', function (){
     // TODO
-     describe('POST /expenditures', function () {
-        it('should return confirmation message and update database', function(done) {
-            let expenditure = {
-                email: '1804094745@qq.com',
-                date: '2018-10-04',
-                payment: 'Cash' ,
-                amount: 10,
-                message: 'glasses',
-                type: 'Others'
-            };
-            chai.request(server)
-                .post('/expenditures')
-                .send(expenditure)
-                .end(function(err, res) {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('message').equal('Expenditure Successfully Added!' );
-                    let expenditure = res.body.data;
-                    expect(expenditure).to.include({message: 'glasses', date: '2018-10-04'});
-                    done();
-                });
-        });
-        after(function  (done) {
-            chai.request(server)
-                .get('/expenditures')
-                .end(function(err, res) {
-                    let result = _.map(res.body, (expenditure) => {
-                        return { payment: expenditure.payment,
-                            amount: expenditure.amount };
-                    }  );
-                    expect(result).to.include( { payment: 'Cash', amount: 10  } );
-                    done();
-                });
-            //chai.request(server)
-            //.delete('/expenditures/fuzzydelete/2018-12')
-            //.end(function(err, res) {
-            //done();
-            //});
-        });  // end-after
-    });
-
-
-
-
-
-    describe('PUT /expenditures/:id/changeExinfo', () => {
-        describe ('when id is valid',function() {
-            it('should return a confirmation message and update database', function (done) {
-                let expenditure = {
-                    email: '1804094745@qq.com',
-                    message: 'latte',
-                    date: '2018-10-01',
-                    payment: 'we chat',
-                    amount: 2.95,
-                    type: "repast"
-                };
-                chai.request(server)
-                    .put('/expenditures/5c06a9d7bc2a6545387ddba2/changeExinfo')
-                    .send(expenditure)
-                    .end(function (err, res) {
-                        expect(res).to.have.status(200);
-                        expect(res.body).to.have.property('Message').equal('Expenditure Successfully Changed!');
-                        let expenditure = res.body.data;
-                        expect(expenditure).to.include({message: 'latte', date: '2018-10-01'});
-                        done();
-                    });
-            });
-            after(function  (done) {
-                chai.request(server)
-                    .get('/expenditures')
-                    .end(function(err, res) {
-                        let result = _.map(res.body, (expenditure) => {
-                            return { message: expenditure.message,
-                                date: expenditure.date };
-                        }  );
-                        expect(result).to.include( { message: 'latte', date: '2018-10-01'  } );
-                        //done();
-                    });
-                let expenditure = {
-                    email: '1804094745@qq.com',
-                    message: 'cup',
-                    date: '2018-10-01',
-                    payment: 'we chat',
-                    amount: 2.95,
-                    type: 'repast'
-                };
-                chai.request(server)
-                    .put('/expenditures/5c06a9d7bc2a6545387ddba2/changeExinfo')
-                    .send(expenditure)
-                    .end(function (err, res) {
-                        done();
-                    });
-            });  // end-after
-        });
-        describe('when id is invalid',function() {
-            it('should return a 404 and a message for invalid expenditure id', function (done) {
-                chai.request(server)
-                    .put('/expenditures/1100001/changeExinfo')
-                    .end(function (err, res) {
-                        expect(res).to.have.status(404);
-                        expect(res.body).to.have.property('Message', 'Sorry! Cannot find the expenditure by this id!');
-                        done();
-                    });
-            });
-        });
-    });
-
-
-
-    describe('DELETE /expenditures/:id', () => {
-        describe ('when id is valid',function(){
-            //let exId;
-            //before(function () {
-            //chai.request(server)
-            //.get('/expenditures')
-            //.end(function(err, res) {
-            //const exId = res.body[12]._id;
-            //});
-            //});
-            it('should return a confirmation message and update database ', function(done) {
-                chai.request(server)
-                    .get('/expenditures')
-                    .end(function(err, res) {
-                        const exId = res.body[4]._id;
-                        chai.request(server)
-                            .delete('/expenditures/' + exId)
-                            .end(function(err, res) {
-                                expect(res).to.have.status(200);
-                                expect(res.body).to.have.property('message','Expenditure Successfully Deleted!' ) ;
-                                done();
-                            });
-                    });
-                //.delete('/expenditures/' + exId)
-                //.end(function(err, res) {
-                //expect(res).to.have.status(200);
-                //expect(res.body).to.have.property('message','Expenditure Successfully Deleted!' ) ;
-                //done();
-                //});
-
-            });
-            after(function  (done) {
-                chai.request(server)
-                    .get('/expenditures')
-                    .end(function(err, res) {
-                        expect(res).to.have.status(200);
-                        expect(res.body).to.be.a('array');
-                        let result = _.map(res.body, function(expenditure) {
-                            return { payment: expenditure.payment,
-                                amount: expenditure.amount };
-                        }  );
-                        //expect(result).to.have.lengthOf(1) ;
-                        //expect(result).to.not.include( { paymenttype: 'Paypal', amount: 1600  } );
-                        expect(result).to.not.include( { payment: 'Cash', amount: 10  } );
-                        done();
-                    });
-            });  // end after
-        });
-        describe('when id is invalid',function(){
-            it('should return a 404 and a message for invalid expenditure id', function(done) {
-                chai.request(server)
-                    .delete('/expenditures/1100001')
-                    .end(function(err, res) {
-                        expect(res).to.have.status(404);
-                        expect(res.body).to.have.property('message','Expenditure NOT DELETED!' ) ;
-                        done();
-                    });
-            });
-
-        });
-
-    });
-
-
-
-
-
-
-
 
     describe('GET /expenditures',  () => {
         it('should return all the expenditures in an array', function(done) {
@@ -376,6 +199,176 @@ describe('Expenditures', function (){
 
 
 
+    describe('POST /expenditures', function () {
+        it('should return confirmation message and update database', function(done) {
+            let expenditure = {
+                email: '1804094745@qq.com',
+                date: '2018-10-04',
+                payment: 'Cash' ,
+                amount: 10,
+                message: 'glasses',
+                type: 'Others'
+            };
+            chai.request(server)
+                .post('/expenditures')
+                .send(expenditure)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message').equal('Expenditure Successfully Added!' );
+                    let expenditure = res.body.data;
+                    expect(expenditure).to.include({message: 'glasses', date: '2018-10-04'});
+                    done();
+                });
+        });
+        after(function  (done) {
+            chai.request(server)
+                .get('/expenditures')
+                .end(function(err, res) {
+                    let result = _.map(res.body, (expenditure) => {
+                        return { payment: expenditure.payment,
+                            amount: expenditure.amount };
+                    }  );
+                    expect(result).to.include( { payment: 'Cash', amount: 10  } );
+                    done();
+                });
+            //chai.request(server)
+            //.delete('/expenditures/fuzzydelete/2018-12')
+            //.end(function(err, res) {
+            //done();
+            //});
+        });  // end-after
+    });
+
+
+
+
+
+    describe('PUT /expenditures/:id/changeExinfo', () => {
+        describe ('when id is valid',function() {
+            it('should return a confirmation message and update database', function (done) {
+                let expenditure = {
+                    email: '1804094745@qq.com',
+                    message: 'latte',
+                    date: '2018-10-01',
+                    payment: 'we chat',
+                    amount: 2.95,
+                    type: "repast"
+                };
+                chai.request(server)
+                    .put('/expenditures/5c06a9d7bc2a6545387ddba2/changeExinfo')
+                    .send(expenditure)
+                    .end(function (err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.have.property('Message').equal('Expenditure Successfully Changed!');
+                        let expenditure = res.body.data;
+                        expect(expenditure).to.include({message: 'latte', date: '2018-10-01'});
+                        done();
+                    });
+            });
+            after(function  (done) {
+                chai.request(server)
+                    .get('/expenditures')
+                    .end(function(err, res) {
+                        let result = _.map(res.body, (expenditure) => {
+                            return { message: expenditure.message,
+                                date: expenditure.date };
+                        }  );
+                        expect(result).to.include( { message: 'latte', date: '2018-10-01'  } );
+                        //done();
+                    });
+                let expenditure = {
+                    email: '1804094745@qq.com',
+                    message: 'cup',
+                    date: '2018-10-01',
+                    payment: 'we chat',
+                    amount: 2.95,
+                    type: 'repast'
+                };
+                chai.request(server)
+                    .put('/expenditures/5c06a9d7bc2a6545387ddba2/changeExinfo')
+                    .send(expenditure)
+                    .end(function (err, res) {
+                        done();
+                    });
+            });  // end-after
+        });
+        describe('when id is invalid',function() {
+            it('should return a 404 and a message for invalid expenditure id', function (done) {
+                chai.request(server)
+                    .put('/expenditures/1100001/changeExinfo')
+                    .end(function (err, res) {
+                        expect(res).to.have.status(404);
+                        expect(res.body).to.have.property('Message', 'Sorry! Cannot find the expenditure by this id!');
+                        done();
+                    });
+            });
+        });
+    });
+
+
+
+    describe('DELETE /expenditures/:id', () => {
+        describe ('when id is valid',function(){
+            //let exId;
+            //before(function () {
+            //chai.request(server)
+            //.get('/expenditures')
+            //.end(function(err, res) {
+            //const exId = res.body[12]._id;
+            //});
+            //});
+            it('should return a confirmation message and update database ', function(done) {
+                chai.request(server)
+                    .get('/expenditures')
+                    .end(function(err, res) {
+                        const exId = res.body[4]._id;
+                        chai.request(server)
+                            .delete('/expenditures/' + exId)
+                            .end(function(err, res) {
+                                expect(res).to.have.status(200);
+                                expect(res.body).to.have.property('message','Expenditure Successfully Deleted!' ) ;
+                                done();
+                            });
+                    });
+                //.delete('/expenditures/' + exId)
+                //.end(function(err, res) {
+                //expect(res).to.have.status(200);
+                //expect(res.body).to.have.property('message','Expenditure Successfully Deleted!' ) ;
+                //done();
+                //});
+
+            });
+            after(function  (done) {
+                chai.request(server)
+                    .get('/expenditures')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.a('array');
+                        let result = _.map(res.body, function(expenditure) {
+                            return { payment: expenditure.payment,
+                                amount: expenditure.amount };
+                        }  );
+                        //expect(result).to.have.lengthOf(1) ;
+                        //expect(result).to.not.include( { paymenttype: 'Paypal', amount: 1600  } );
+                        expect(result).to.not.include( { payment: 'Cash', amount: 10  } );
+                        done();
+                    });
+            });  // end after
+        });
+        describe('when id is invalid',function(){
+            it('should return a 404 and a message for invalid expenditure id', function(done) {
+                chai.request(server)
+                    .delete('/expenditures/1100001')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(404);
+                        expect(res.body).to.have.property('message','Expenditure NOT DELETED!' ) ;
+                        done();
+                    });
+            });
+
+        });
+
+    });
 
 
 
